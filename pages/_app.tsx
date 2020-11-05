25c4f4d9a02a9ee4834d2289381f304ab8e12680
muktiarafi <muktiarafi@gmail.com>
2020-11-05 12:35:22 +0700

    grid styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/tailwind.css";
 
 const AppComponent = ({ Component, pageProps }) => {
   return (
-    <div className="bg-gray-800 p-8 h-screen">
+    <div className="bg-gray-800 p-8 h-full">
       <Component {...pageProps} />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,38 @@
-import Image from "next/image";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    (document.getElementById("text") as HTMLTextAreaElement).addEventListener(
+      "keydown",
+      function (e) {
+        if (e.key == "Tab") {
+          e.preventDefault();
+          let start = this.selectionStart;
+          let end = this.selectionEnd;
+
+          this.value =
+            this.value.substring(0, start) + "\t" + this.value.substring(end);
+          this.selectionStart = this.selectionEnd = start + 1;
+        }
+      }
+    );
+  }, []);
+
   return (
-    <div>
-      <div className="flex flex-row justify-center">
-        <Image
-          className="h-auto row-span-3 col-span-1 m-8"
-          src="/paper.jpg"
-          alt="paper"
-          width={600}
-          height={735}
-        />
-        <textarea className="h-auto ml-8 border rounded px-2 py-2" cols={50} />
-      </div>
+    <div className="grid grid-rows-3 grid-flow-col gap-4 justify-center">
+      <img
+        src="/paper.jpg"
+        alt="paper"
+        className="h-screen row-span-4 rounded"
+      />
+      <textarea
+        id="text"
+        className="ml-8 row-span-3 col-span-2 rounded px-4 py-2"
+        cols={50}
+      />
+      <button className="ml-8 row-span-1 col-span-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Simpan Gambar
+      </button>
     </div>
   );
 };
